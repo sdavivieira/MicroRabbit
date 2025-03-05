4b24d3e4cf0d53b4da8b60a7b321742123ff73e4
Davi Vieira <100722433+sdavivieira@users.noreply.github.com>
2025-03-05 11:41:25 -0300

    commit IoC

---
+++ MicroRabbit.Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,19 @@
+﻿using MicroRabbit.Domain.Core.Bus;
+using MicroRabbit.Infra.Bus;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroRabbit.Infra.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            services.AddTransient<IEventBus, RabbitMQBus>();
+        }
+    }
+}
